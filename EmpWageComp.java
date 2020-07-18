c5afb261f11808839429e407b74b11980c3390b2
Rajeev <rajeevsuchiha@gmail.com>
2020-07-18 15:03:25 +0530

    Added: Emp Wage Computation file is added

---
+++ EmpWageComp.java
@@ -0,0 +1,9 @@
+public class EmpWageComp {
+
+        public static void main(String[] args) {
+
+                System.out.println("**********Welcome to Employee Wage Computation Problem**********");
+
+        }
+
+}
