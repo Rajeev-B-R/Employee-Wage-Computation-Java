fec7d1ca6ce9f3272112d9dd51ca7ba907e10010
Rajeev <rajeevsuchiha@gmail.com>
2020-07-18 17:04:18 +0530

    Added: Code added to calculate total wages for month

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -6,23 +6,28 @@ public class EmpWageComp {
 		int isFullTime = 1;
 		int isPartTime = 2;
 		int wagePerHr = 20;
-		int empCheck = (int)Math.floor(Math.random() * 10 ) % 3;
 		int dailyWage = 0;
 		int empHrs=0;
-		
-		switch (empCheck)
-		{
-		case 0:
-			empHrs=0;
-			break;
-		case 1:
-			empHrs=8;
-			break;
-		case 2:
-			empHrs=16;
-			break;
-		}
-		dailyWage=wagePerHr*empHrs;
-		System.out.println("Daily wage of Employee:" + dailyWage);
-        }
+		int totalWage=0;
+	
+		for(int i=0; i<20; i++) {
+			int empCheck = (int)Math.floor(Math.random() * 10 ) % 3;
+			System.out.println(empCheck);
+			switch (empCheck)
+			{
+				case 0:
+					empHrs=0;
+					break;
+				case 1:
+					empHrs=8;
+					break;
+				case 2:
+					empHrs=16;
+					break;
+			}
+			dailyWage=wagePerHr*empHrs;
+			totalWage=totalWage+dailyWage;
+        	}
+		System.out.println("Total wage of Employee:" + totalWage);
+	}
 }
