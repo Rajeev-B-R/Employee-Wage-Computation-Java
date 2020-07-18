685969be8122b873cee5f4a930740b218e9e1d0c
Rajeev <rajeevsuchiha@gmail.com>
2020-07-18 15:34:18 +0530

    Added: Code to check for part time Employee and wage added

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -4,21 +4,26 @@ public class EmpWageComp {
 
                 System.out.println("**********Welcome to Employee Wage Computation Problem**********");
 		int isFullTime = 1;
-		int fullDayHr = 8;
+		int isPartTime = 2;
+		int fullDayHr = 16;
+		int partTimeHr = 8;
 		int wagePerHr = 20;
 		double empCheck = Math.floor(Math.random() * 10 ) % 2;
 		int dailyWage = 0;
 		
 		if (empCheck == isFullTime) {
 
-			System.out.println("Employee present");
+			System.out.println("Employee -> Full time present");
 			dailyWage = wagePerHr*fullDayHr;
 		}
+		else if (empCheck == isPartTime) {
+
+                        System.out.println("Employee -> Part time present");
+                        dailyWage = wagePerHr*partTimeHr;
+                }
 		else {
 			System.out.println("Employee absent");
 		}
 		System.out.println("Daily wage of Employee:" + dailyWage);
-
         }
-
 }
