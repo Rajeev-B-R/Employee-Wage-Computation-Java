cd30987f477bd7c458fa33916d7138b30daf21f0
Rajeev <rajeevsuchiha@gmail.com>
2020-07-20 13:02:05 +0530

    Modified: Code modified to calculate Total Wage using class

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -3,38 +3,48 @@ public class EmpWageComp {
         public static void main(String[] args) {
 
                 System.out.println("**********Welcome to Employee Wage Computation Problem**********");
-		int isFullTime = 1;
-		int isPartTime = 2;
-		int wagePerHr = 20;
-		int dailyWage = 0;
-		int empHrs=0;
-		int totalWage=0;
-		int totalWorkingHrs = 0;
-		int totalWorkingDays = 0;
-		
-		while(totalWorkingHrs <= 100 && totalWorkingDays < 20)
+		public static final int ispartTime=1;
+		public static final int isFullTime=2;
+		public static final int wagePerHr=20;
+		public static final int maxWorkingDays=20;
+		public static final int maxHrsInMonth=100;
+    
+		public static int calcEmpWage()
 		{
-			totalWorkingDays++;
-			int empCheck = (int)Math.floor(Math.random() * 10)%3;
-			switch (empCheck)
-			{
-				case 0:
-					empHrs=0;
-					break;
-				case 1:
-					empHrs=4;
-					break;
-				case 2:
-					empHrs=8;
-					break;
-			}
-			
-			totalWorkingHrs+=empHrs;
-			System.out.println("Day: " +totalWorkingDays+   "Employee Hours: "+empHrs);
-		}
-		totalWage=totalWorkingHrs*wagePerHr;
-		System.out.println("Total wage of employee:"+totalWage);
+			int empHrs=0;
+			int totalWorkingHrs=0;
+			int totalWorkingDays=0;
+			int totalWage=0;
 		
-	}
+			while(totalWorkingHrs<=maxHrsInMonth && totalWorkingDays<maxWorkingDays)
+			{
+				int empWage=0;
+				totalWorkingDays++;
+				int empCheck=(int) Math.floor(Math.random()*10)%3;
+				switch(empCheck)
+				{
+					case ispartTime:
+						empHrs=4;
+						break;
+					case isFullTime:
+						empHrs=8;
+						break;
+					default:
+						empHrs=0;
+				}
+				totalWorkingHrs+=empHrs;
+				empWage=empHrs*wagePerHr;
+				System.out.println("Day:"+totalWorkingDays+"  Employee Hours:"+empHrs+"  Employee wage: "+empWage);
+			}
+			totalWage=totalWorkingHrs*wagePerHr;
+			System.out.println();
+			System.out.println("Total Employee Hours:"+totalWorkingHrs+"	 Total Employee Wage:"+totalWage);
+			return totalWage;
+		}
 
+	public static void main(String[] args)
+	{
+		calcEmpWage();
+	}
 }
+
